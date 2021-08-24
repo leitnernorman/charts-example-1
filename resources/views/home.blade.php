5a48c58d8db17972f1111c28b383108458f38c22
Norman Leitner <leitnernorman7@gmail.com>
2021-08-24 11:14:32 +0200

    ch-2

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,12 +7,38 @@
 </head>
 
 <body>
-    <h1>Highcharts in Laravel 8 Example</h1>
-    <div id="container"></div>
-    <div id="container2"></div>
+<!-- <div class="buttons">
+  <button id="uncertified" class="active">
+  Uncertified/Conventional
+  </button>
+  <button id="traceable">
+  Fully/partially traceable
+  </button>
+  <button id="certified">
+  Certified green
+  </button>
+</div>-->
+<div id="container1" style="display:none;"></div>
+
+<figure class="highcharts-figure">
+  <div id="container"></div>
+  <!-- <p class="highcharts-description">
+    A variation of a 3D pie chart with an inner radius added.
+    These charts are often referred to as donut charts.
+  </p> -->
+</figure>
+<figure class="highcharts-figure">
+  <div id="container3"></div>
+</figure>
+<figure class="highcharts-figure">
+  <div id="container4"></div>
+</figure>
+<div id="container2"></div>
 </body>
 
 <script src="https://code.highcharts.com/highcharts.js"></script>
+<script src="https://code.highcharts.com/highcharts-3d.js"></script>
+<script src="https://code.highcharts.com/modules/accessibility.js"></script>
 
 <script type="text/javascript">
     var userData = <?php echo json_encode($userData)?>;
@@ -21,38 +47,43 @@
         type: 'column'
     },
     title: {
-        text: 'Monthly Average Rainfall'
+        text: 'Largest palm oil handling companies'
     },
     subtitle: {
-        text: 'Source: WorldClimate.com'
+        text: 'Source: data.js :)'
     },
     xAxis: {
         categories: [
-        'Jan',
-        'Feb',
-        'Mar',
-        'Apr',
-        'May',
-        'Jun',
-        'Jul',
-        'Aug',
-        'Sep',
-        'Oct',
-        'Nov',
-        'Dec'
+        'Wilmar',
+        'GAR',
+        'Sime Darby Plantation',
+        'IOI',
+        'KLK',
+        'Bunge',
+        'Unilever',
+        'Savola Group',
+        'P&G',
+        'Nestle',
+        'Kao',
+        'RB',
+        'Yum! Brands',
+        'Colgate',
+        'Mondelez',
+        'Nissin Foods',
+        'Rest of buyers'
         ],
         crosshair: true
     },
     yAxis: {
         min: 0,
         title: {
-        text: 'Rainfall (mm)'
+        text: 'Pailm oil buy/produce (ton)'
         }
     },
     tooltip: {
         headerFormat: '<span style="font-size:10px">{point.key}</span><table>',
         pointFormat: '<tr><td style="color:{series.color};padding:0">{series.name}: </td>' +
-        '<td style="padding:0"><b>{point.y:.1f} mm</b></td></tr>',
+        '<td style="padding:0"><b>{point.y:.1f} ton</b></td></tr>',
         footerFormat: '</table>',
         shared: true,
         useHTML: true
@@ -64,25 +95,21 @@
         }
     },
     series: [{
-        name: 'Tokyo',
-        data: [49.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
+        name: 'Uncertified/Conventional',
+        data: [23246764, 7615904, 1201485, 1387979, 836294, 893688, 326180, 538766, 281104, 219647, 323900, 159728, 10241, 42745, 5781, 91106, 353625]
 
     }, {
-        name: 'New York',
-        data: [83.6, 78.8, 98.5, 93.4, 106.0, 84.5, 105.0, 104.3, 91.2, 83.5, 106.6, 92.3]
+        name: 'Fully/partially traceable',
+        data: [1053236, 431575, 2037439, 536104, 249596, 166312, 698641, 33019, 182191, 130617, 87500, 12700, 52583, 67490, 4409, 22903, 827093]
 
     }, {
-        name: 'London',
-        data: [48.9, 38.8, 39.3, 41.4, 47.0, 48.3, 59.0, 59.6, 52.4, 65.2, 59.3, 51.2]
-
-    }, {
-        name: 'Berlin',
-        data: [42.4, 33.2, 34.5, 39.7, 52.6, 75.5, 57.4, 60.4, 47.6, 39.1, 46.8, 51.1]
+        name: 'Certified green',
+        data: [0, 0, 0, 20326, 114667, 0, 13356, 0, 0, 75250, 10000, 0, 94952, 28255, 280127, 0, 363497]
 
     }]
     });
 
-    Highcharts.chart('container', {
+    Highcharts.chart('container1', {
         title: {
             text: 'New User Growth, 2020'
         },
@@ -129,6 +156,137 @@
         }
     });
 
+    Highcharts.chart('container', {
+  chart: {
+    type: 'pie',
+    options3d: {
+      enabled: true,
+      alpha: 45
+    }
+  },
+  title: {
+    text: 'Uncertified/Conventional palm oil production/buy'
+  },
+  subtitle: {
+    text: 'Source: data.js :)'
+  },
+  plotOptions: {
+    pie: {
+      innerSize: 100,
+      depth: 45
+    }
+  },
+  series: [{
+    name: 'Palm oil handling in ton',
+    data: [
+      ['Wilmar', 23246764],
+      ['GAR', 7615904],
+      ['Sime Darby Plantation', 1201485],
+      ['IOI', 1387979],
+      ['KLK', 836294],
+      ['Bunge', 893688],
+      ['Unilever', 326180],
+      ['Savola Group', 538766],
+      ['P&G', 281104],
+      ['Nestle', 219647],
+      ['Kao', 323900],
+      ['RB', 159728],
+      ['Yum! Brands', 10241],
+      ['Colgate', 42745],
+      ['Mondelez', 5781],
+      ['Nissin Foods', 91106],
+      ['Rest of buyers', 353625]
+    ]
+  }]
+});
+
+Highcharts.chart('container3', {
+  chart: {
+    type: 'pie',
+    options3d: {
+      enabled: true,
+      alpha: 45
+    }
+  },
+  title: {
+    text: 'Fully/partially traceable palm oil production/buy'
+  },
+  subtitle: {
+    text: 'Source: data.js :)'
+  },
+  plotOptions: {
+    pie: {
+      innerSize: 100,
+      depth: 45
+    }
+  },
+  series: [{
+    name: 'Palm oil handling in ton',
+    data: [
+      ['Wilmar', 1053236],
+      ['GAR', 431575],
+      ['Sime Darby Plantation', 2037439],
+      ['IOI', 536104],
+      ['KLK', 249596],
+      ['Bunge', 166312],
+      ['Unilever', 698641],
+      ['Savola Group', 33019],
+      ['P&G', 182191],
+      ['Nestle', 130617],
+      ['Kao', 87500],
+      ['RB', 12700],
+      ['Yum! Brands', 52583],
+      ['Colgate', 67490],
+      ['Mondelez', 4409],
+      ['Nissin Foods', 22903],
+      ['Rest of buyers', 827093]
+    ]
+  }]
+});
+
+Highcharts.chart('container4', {
+  chart: {
+    type: 'pie',
+    options3d: {
+      enabled: true,
+      alpha: 45
+    }
+  },
+  title: {
+    text: 'Certified green palm oil production/buy'
+  },
+  subtitle: {
+    text: 'Source: data.js :)'
+  },
+  plotOptions: {
+    pie: {
+      innerSize: 100,
+      depth: 45
+    }
+  },
+  series: [{
+    name: 'Palm oil handling in ton',
+    data: [
+      ['Wilmar', 0],
+      ['GAR', 0],
+      ['Sime Darby Plantation', 0],
+      ['IOI', 20326],
+      ['KLK', 114667],
+      ['Bunge', 0],
+      ['Unilever', 13356],
+      ['Savola Group', 0],
+      ['P&G', 0],
+      ['Nestle', 75250],
+      ['Kao', 10000],
+      ['RB', 0],
+      ['Yum! Brands', 94952],
+      ['Colgate', 28255],
+      ['Mondelez', 280127],
+      ['Nissin Foods', 0],
+      ['Rest of buyers', 363497]
+    ]
+  }]
+});
 </script>
 
 </html>
